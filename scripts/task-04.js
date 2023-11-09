e9fdff6ea5093dfe8e0703d81242fdb2845f6767
Denys Strashniuk <79088186+lordponchik@users.noreply.github.com>
2023-11-09 13:43:10 +0100

    add task-04

---
+++ scripts/task-04.js
@@ -0,0 +1,10 @@
+console.log('task-04');
+console.log('');
+
+import users from '../users.js';
+
+const getInactiveUsers = users => {
+  return users.filter(user => !user.isActive);
+};
+
+console.log(getInactiveUsers(users)); // [объект Moore Hensley, объект Ross Vazquez, объект Blackburn Dotson]
